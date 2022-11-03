2a6c1d241661916e5c8c9e4e77361da2640cef20
Clinton <clitnonuchendu@gmail.com>
2022-11-03 09:32:57 -0400

    Initial Commit

---
+++ LearnGit/LearnGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitApp.swift
+//  LearnGit
+//
+//  Created by Clinton Uchendu on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
